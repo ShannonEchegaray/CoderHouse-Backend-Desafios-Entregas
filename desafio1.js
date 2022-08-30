83d25cc5d0aae7d4516ff2be1169c2b772208aa0
Shannon Echegaray <shannonechegaray@gmail.com>
2022-08-30 00:16:44 -0300

    default books and pets

--- desafio1.js
+++ desafio1.js
@@ -1,29 +1,29 @@
 class Usuario{
-    constructor(nombre, apellido){
+    constructor(nombre, apellido, libros, mascotas){
         this.nombre = nombre;
         this.apellido = apellido;
-        this.libros = []
-        this.mascotas = []
+        this.libros = libros || [];
+        this.mascotas = mascotas || [];
     }
 
     getFullName(){
-        return `${this.nombre} ${this.apellido}`
+        return `${this.nombre} ${this.apellido}`;
     }
 
     addMascota(string){
-        this.mascotas.push(string)
+        this.mascotas.push(string);
     }
 
     countMascotas(){
-        return this.mascotas.length
+        return this.mascotas.length;
     }
 
     addBook(nombre, autor){
-        this.libros.push({nombre, autor})
+        this.libros.push({nombre, autor});
     }
 
     getBookNames(){
-        return this.libros.map(({nombre}) => nombre)
+        return this.libros.map(({nombre}) => nombre);
     }
 }
 
@@ -36,3 +36,8 @@ console.log("Mis libros son: " + usuario1.getBookNames());
 usuario1.addBook("El señor de los anillos", "J. R. R. Tolkien");
 usuario1.addBook("Legend", "Marie Lu");
 console.log("Mis libros son: " + usuario1.getBookNames());
+
+const usuario2 = new Usuario("Carlos", "Lopez", [{nombre: "El señor de los anillos", autor: "J. R. R. Tolkien"}], ["Rio", "Grisbie"])
+console.log(`Hola mi nombre es ${usuario2.getFullName()}`);
+console.log("Mascotas: " + usuario2.countMascotas());
+console.log("Mis libros son: " + usuario2.getBookNames());
