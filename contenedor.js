369a08d2b0949907610333b3b706b31977922bd7
Shannon Echegaray <shannonechegaray@gmail.com>
2022-09-14 09:19:47 -0300

    Creando server Express y subiendo a glitch

--- contenedor.js
+++ contenedor.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 module.exports = class Contenedor{
     constructor(nombreArchivo){
-        this.ruta = `./${nombreArchivo}`
+        this.ruta = `${nombreArchivo}`
     }
 
     async save(producto){
