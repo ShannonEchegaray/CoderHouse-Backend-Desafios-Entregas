6f5cad23c84bfb92f94c8e591377ff424a5d686c
Shannon Echegaray <shannonechegaray@gmail.com>
2022-09-28 14:24:42 -0300

    Desafio numero 5

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(express.static(path.join(__dirname, "static")))
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
 
-app.use("/api", router)
+app.use(router)
 
 app.use((error, req, res, next) => {
   if(error.statusCode){
-    console.log("Estoy aca")
-    return res.status(error.statusCode).json({error: error.message})
+    return res.status(error.statusCode).render("error", {...error, message: error.message})
   }
   console.log(error)
   res.status(500).json({error: "Somethings brokes..."})
@@ -24,4 +24,5 @@ app.use((error, req, res, next) => {
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
+  console.log("Environment: " + process.env.NODE_ENV)
 })
