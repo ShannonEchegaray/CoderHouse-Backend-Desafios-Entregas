a1a30970cf56f848027ce361519a2930bb489ac3
Shannon Echegaray <shannonechegaray@gmail.com>
2022-11-16 16:44:48 -0300

    Estructura del desafio-10

---
+++ routes/login.js
@@ -0,0 +1,17 @@
+import {Router} from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+
+});
+
+router.post("/login", (req, res) => {
+
+});
+
+router.get("/logout", (req, res) => {
+
+});
+
+export default router;
--- server.js
+++ server.js
@@ -3,35 +3,45 @@ import express from "express";
 import {initServer, emit} from "./socket.js";
 import http from "http";
 import bodyParser from "body-parser";
-import path from "path";
-import router from "./routes/products.js"
+import expressSession from "express-session";
+import MongoStore from "connect-mongo";
+import router from "./routes/products.js";
 const app = express();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static("./static"))
-app.set("views", "./views")
-app.set("view engine", "pug")
+app.use(express.json());
+app.use(expressSession({
+  store: new MongoStore({
+    mongoUrl: "mongodb+srv://backend:<password>@cluster0.rdtbnd0.mongodb.net/?retryWrites=true&w=majority",
+    ttl: 10
+  }),
+  secret: "shhh",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("./static"));
+app.set("views", "./views");
+app.set("view engine", "pug");
 
-app.use("api", router)
+app.use("/", router);
 
 app.use((error, req, res, next) => {
   if(error.statusCode){
-    return res.status(error.statusCode).send(`Error ${error.statusCode}`)
+    return res.status(error.statusCode).send(`Error ${error.statusCode}`);
   }
-  console.log(error)
-  res.status(500).json({error: "Somethings brokes..."})
+  console.log(error);
+  res.status(500).json({error: "Somethings brokes..."});
 })
 
 // listen for requests :)
 
-const server = http.createServer(app)
-initServer(server)
+const server = http.createServer(app);
+initServer(server);
 
 server.listen(PORT, function() {
   console.log("Your app is listening on port " + PORT);
-  console.log("Environment: " + process.env.NODE_ENV)
+  console.log("Environment: " + process.env.NODE_ENV);
 })
