2a44d2388d86e908fddcd318b84c721b79a53309
Shannon Echegaray <shannonechegaray@gmail.com>
2022-12-14 21:41:38 -0300

    Haciendo test de carga y profiling

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,16 @@ router.use("/api", routerAPI);
 router.get("/info", (req, res) => {
     try {
         logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+        console.log({
+            cpus: os.cpus().length,
+            argv: process.argv.slice(2),
+            platform: process.platform,
+            version: process.version,
+            rss: process.memoryUsage(),
+            cwd: process.cwd(),
+            pe: process.execPath,
+            pid: process.pid,
+        });
         res.json({
             cpus: os.cpus().length,
             argv: process.argv.slice(2),
